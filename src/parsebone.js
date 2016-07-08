3dcee122af980fd671902819b823c06f3dda7e84
Luís Rodrigues <netslider25@gmail.com>
2016-07-08 10:14:18 +0100

    Added module export

--- src/parsebone.js
+++ src/parsebone.js
@@ -1,3 +1,5 @@
+/* global define */
+
 /*!
  * Parse JavaScript SDK
  * Version: 1.5.0
@@ -10,36 +12,53 @@
  * Copyright 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
  * Released under the MIT license.
  */
+var moduleExport
 
-/* eslint-disable semi */
-(function (root) {
-  root.Parse = root.Parse || {}
-  root.Parse.VERSION = 'js1.5.0'
-}(this));
-/* eslint-enable semi */
+if ((typeof define === 'function' && define.amd) || (typeof module === 'object' && module.exports)) {
+  moduleExport = {}
+} else {
+  moduleExport = this
+}
 
-// =require underscore/underscore.js
-// =require parse.js
-// =require storage.js
-// =require analytics.js
-// =require config.js
-// =require error.js
-// =require events.js
-// =require geopoint.js
-// =require acl.js
-// =require op.js
-// =require relation.js
-// =require promise.js
-// =require file.js
-// =require object.js
-// =require role.js
-// =require collection.js
-// =require view.js
-// =require user.js
-// =require session.js
-// =require query.js
-// =require facebookutils.js
-// =require history.js
-// =require router.js
-// =require cloud.js
-// =require push.js
+;(function () {
+  this.Parse = {}
+  this.Parse.VERSION = 'js1.5.0'
+
+  /* eslint-disable semi */
+  ; // Added so that the syntax for the self executing functions in the Underscore module is correct
+  /* eslint-enable semi */
+
+  // =require underscore/underscore.js
+  // =require parse.js
+  // =require storage.js
+  // =require analytics.js
+  // =require config.js
+  // =require error.js
+  // =require events.js
+  // =require geopoint.js
+  // =require acl.js
+  // =require op.js
+  // =require relation.js
+  // =require promise.js
+  // =require file.js
+  // =require object.js
+  // =require role.js
+  // =require collection.js
+  // =require view.js
+  // =require user.js
+  // =require session.js
+  // =require query.js
+  // =require facebookutils.js
+  // =require history.js
+  // =require router.js
+  // =require cloud.js
+  // =require push.js
+
+  this._ = this.Parse._
+}).call(moduleExport)
+
+if (typeof define === 'function' && define.amd) {
+  define(moduleExport)
+} else if (typeof module === 'object' && module.exports) {
+  module.exports = moduleExport
+}
